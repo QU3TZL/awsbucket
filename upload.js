d83c7e8ddc7eafc5254bf17a27d0a6565e7dc569
Quetzl <112727259+QU3TZL@users.noreply.github.com>
2023-07-10 16:53:25 -0500

    Create upload.js

---
+++ upload.js
@@ -0,0 +1,21 @@
+const AWS = require('./config');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
+const s3 = new AWS.S3();
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+});
+
+module.exports = upload;
