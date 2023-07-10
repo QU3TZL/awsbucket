a7a3e2c12083277646fdb7d1d38921214863a95e
Quetzl <112727259+QU3TZL@users.noreply.github.com>
2023-07-10 17:26:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const upload = require('./upload');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Add this line to use the CORS middleware
+app.use(cors({
+  origin: 'https://tenfold.onrender.com'
+}));
 
 app.post('/upload', upload.array('file', 10), (req, res) => {
   res.send('Files uploaded successfully');
