871bbb3f10e812c7ae496c1a7fce06f49c8f3095
Quetzl <112727259+QU3TZL@users.noreply.github.com>
2023-07-15 10:56:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const SftpClient = require('ssh2-sftp-client');
+const fs = require('fs');
 const config = require('./config');
 
 const app = express();
@@ -12,24 +13,21 @@ app.post('/upload/:folder', async (req, res) => {
     return res.status(400).send('No files were uploaded.');
   }
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let sampleFile = req.files.filepond;
+  let sampleFile = req.files.sampleFile;
   let folder = req.params.folder;
 
-  // Use the mv() method to place the file somewhere on your server
   sampleFile.mv(`/tmp/${sampleFile.name}`, async function(err) {
     if (err)
       return res.status(500).send(err);
 
-    // After the file is saved locally, transfer it to the Render disk
     let sftp = new SftpClient();
     try {
       await sftp.connect({
         host: config.render.host,
         username: config.render.username,
-        password: config.render.password
+        privateKey: fs.readFileSync(process.env.PRIVATE_KEY_PATH) // Read the private key from the path specified in the environment variable
       });
-      await sftp.mkdir(`${config.render.diskPath}/${folder}`, true); // The second parameter set to true makes the function recursive (it will create parent directories if they don't exist)
+      await sftp.mkdir(`${config.render.diskPath}/${folder}`, true);
       await sftp.put(`/tmp/${sampleFile.name}`, `${config.render.diskPath}/${folder}/${sampleFile.name}`);
       res.send('File uploaded and transferred to Render disk!');
     } catch (err) {
